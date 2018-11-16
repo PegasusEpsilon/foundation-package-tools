28fd0e162dffd57fbcc4a2df8b443c1ba8bcc8af
Pegasus Epsilon <pegasus@pimpninjas.org>
2018-11-16 13:41:02 -0600

    first draft of complete(?) remantler

--- remantle.c
+++ remantle.c
@@ -1,5 +1,7 @@
+#define _POSIX_C_SOURCE 200112L
+#define _XOPEN_SOURCE 500
 #include <stdio.h>  	/* FILE, printf() */
-#include <stdlib.h> 	/* exit() */
+#include <stdlib.h> 	/* exit(), mkstemp() */
 #include <stdint.h> 	/* uint32_t */
 #include <stdbool.h>	/* bool, true, false */
 #include <stdarg.h> 	/* va_list, va_start(), vprintf(), va_end() */
@@ -12,7 +14,9 @@
 
 #include "utils.h"  	/* die(), debug(), fread(), chdir() */
 
-uint32_t build_index (FILE *pkg, off_t data_offset) {
+#define BLOCK 8192
+
+uint32_t pack (FILE *idx, FILE *dat, off_t data_offset, uint32_t nested) {
 	struct stat statbuf;
 	uint32_t file_count = 0, entry_count, subdir_file_count = 0;
 	DIR *dir = opendir(".");
@@ -20,48 +24,58 @@ uint32_t build_index (FILE *pkg, off_t data_offset) {
 	// every directory entry in the newer format, including root directory
 	// starts with file count, so, reserve uint32_t for file count
 	// and fill it in at the end
-	int file_count_offset = ftell(pkg);
-	fseek(pkg, sizeof(uint32_t), SEEK_CUR);
+	int file_count_offset = ftell(idx);
+	fseek(idx, sizeof(uint32_t), SEEK_CUR);
 
 	// every directory except the root directory has a skip offset immediately
 	// after the file count. swapping these two values would make this much
 	// simpler...
 
-	int subdir = data_offset;
-	if (subdir)
-		fseek(pkg, sizeof(uint32_t), SEEK_CUR);
+	if (nested)
+		fseek(idx, sizeof(uint32_t), SEEK_CUR);
 
 	for (;;) {
 		struct dirent *entry = readdir(dir);
 		if (NULL == entry) break;
 		if ('.' == *entry->d_name) continue;
-		if (stat(entry->d_name, &statbuf))
-			fail("failed to stat() directory entry");
+		FILE *src = fopen(entry->d_name, "rb");
+		if (fstat(fileno(src), &statbuf))
+			fail("failed to stat() open filehandle");
 		entry_count++;
 		uint32_t len = strlen(entry->d_name);
-		fwrite(&len, sizeof(len), 1, pkg);
-		fwrite(entry->d_name, len, 1, pkg);
+		fwrite(&len, sizeof(len), 1, idx);
+		fwrite(entry->d_name, len, 1, idx);
 		if (S_ISDIR(statbuf.st_mode)) {
+			debug(nested, "Directory: \"%s\"\n", entry->d_name);
+			fclose(src);
 			chdir(entry->d_name);
-			fwrite("\1", sizeof(bool), 1, pkg);
-			subdir_file_count += build_index(pkg, data_offset);
+			fwrite("\1", sizeof(bool), 1, idx);
+			subdir_file_count += pack(idx, dat, data_offset, nested + 1);
 			chdir("..");
 			continue;
 		}
-		fwrite("\0", sizeof(bool), 1, pkg);
-		fwrite((uint32_t *)&data_offset, sizeof(uint32_t), 1, pkg);
-		fwrite((uint32_t *)&statbuf.st_size, sizeof(uint32_t), 1, pkg);
-//		printf("File: \"%s\" offset 0x%lX size %ld\n", entry->d_name, data_offset, statbuf.st_size);
+		size_t readbytes;
+		uint8_t buf[BLOCK];
+		do {
+			readbytes = fread(buf, 1, BLOCK, src);
+			fwrite(buf, 1, readbytes, dat);
+		} while (BLOCK == readbytes);
+		fclose(src);
+		fwrite("\0", sizeof(bool), 1, idx);
+		fwrite((uint32_t *)&data_offset, sizeof(uint32_t), 1, idx);
+		fwrite((uint32_t *)&statbuf.st_size, sizeof(uint32_t), 1, idx);
+		debug(nested, "File: \"%s\" offset 0x%lX size %ld\n", entry->d_name, data_offset, statbuf.st_size);
 		data_offset += statbuf.st_size;
 		file_count++;
 	}
 	// fill in file count that we reserved earlier
-	off_t entry_end_offset = ftell(pkg);
-	fseek(pkg, file_count_offset, SEEK_SET);
-	fwrite((uint32_t *)&entry_count, sizeof(uint32_t), 1, pkg);
+	off_t entry_end_offset = ftell(idx);
+	fseek(idx, file_count_offset, SEEK_SET);
+	fwrite((uint32_t *)&entry_count, sizeof(uint32_t), 1, idx);
 	// fill in the directory entry skip offset, too, if we're in a subdirectory
-	if (subdir) fwrite((uint32_t *)&entry_end_offset, sizeof(uint32_t), 1, pkg);
-	fseek(pkg, entry_end_offset, SEEK_SET);
+	if (nested)
+		fwrite((uint32_t *)&entry_end_offset, sizeof(uint32_t), 1, idx);
+	fseek(idx, entry_end_offset, SEEK_SET);
 	return file_count + subdir_file_count;
 }
 
@@ -76,9 +90,20 @@ int main (int argc, char **argv) {
 		fail("failed to stat() source directory");
 	if (!S_ISDIR(statbuf.st_mode))
 		die("refusing to remantle a non-directory");
+
 	printf("Creating package %s from directory %s...\n", filename, argv[1]);
-	FILE *pkg = fopen(filename, "w");
+	FILE *pkg = fopen(filename, "wb");
+
+	// mangle the filename into a temp file name
+	size_t len = strlen(filename);
+	for (size_t i = 1; i <= 6; i++) filename[len-i] = 'X';
+	// so we can make a temp file out of it
+	int tmpfd = mkstemp(filename);
+	unlink(filename); // early unlink, our buffer is now a ghost
+	FILE *tmp = fdopen(tmpfd, "wb");
+	// since we don't need it later anyway
 	free(filename);
+
 	chdir(argv[1]);
 	// set up header fields
 	// uint32_t file data offset (must index first)
@@ -86,11 +111,23 @@ int main (int argc, char **argv) {
 	// so our first entry offset is +4 +4 = 8
 	// and we skip fields to be populated later
 	fseek(pkg, 8, SEEK_SET);
-	uint32_t total_files = build_index(pkg, 0);
+	uint32_t total_files = pack(pkg, tmp, 0, 0);
 	off_t data_offset = ftell(pkg);
+
+	// copy temp file into the package
+	fseek(tmp, 0, SEEK_SET);
+	uint8_t buf[BLOCK];
+	size_t readbytes;
+	do {
+		readbytes = fread(buf, 1, BLOCK, tmp);
+		fwrite(buf, 1, readbytes, pkg);
+	} while (BLOCK == readbytes);
+	fclose(tmp);
+	close(tmpfd);
 	fseek(pkg, 0, SEEK_SET);
 	fwrite((uint32_t *)&data_offset, sizeof(uint32_t), 1, pkg);
 	fwrite(&total_files, sizeof(total_files), 1, pkg);
 	fclose(pkg);
+	fclose(tmp);
 	return 0;
 }
